0969cee47ccf5480c764e7d249bdb46bea006514
StasKrivosheya <stask49@gmail.com>
2023-05-01 09:41:56 +0300

    Add generic repository interface and UserRepository as implementation, refactor DbProvider

--- lib/src/helpers/database.dart
+++ lib/src/helpers/database.dart
@@ -9,7 +9,6 @@ class DBProvider {
   DBProvider._();
 
   static const String _dBName = "contacts.db";
-  static const String _userTableName = "User";
 
   static final DBProvider instance = DBProvider._();
 
@@ -30,55 +29,13 @@ class DBProvider {
     const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
     const textType = 'TEXT NOT NULL';
 
-    await db.execute('CREATE TABLE $_userTableName ('
+    await db.execute('CREATE TABLE ${UserFields.databaseTableName} ('
         '${UserFields.id} $idType,'
         '${UserFields.login} $textType UNIQUE,'
         '${UserFields.password} $textType'
         ')');
-  }
 
-  Future<int> createUser(User user) async {
-    final db = await instance.database;
-    return db.insert(_userTableName, user.toJson());
-  }
-
-  Future<User?> readUser(String login) async {
-    final db = await instance.database;
-    User? result;
-
-    final maps = await db.query(_userTableName,
-        columns: UserFields.allFieldsNames,
-        where: '${UserFields.login} = ?',
-        whereArgs: [login]);
-
-    if (maps.isNotEmpty) {
-      result = User.fromJson(maps.first);
-    }
-
-    return result;
-  }
-
-  Future<List<User>> readAllUsers() async {
-    final db = await instance.database;
-
-    const orderBy = '${UserFields.login} ASC';
-    final result = await db.query(_userTableName, orderBy: orderBy);
-
-    return result.map((json) => User.fromJson(json)).toList();
-  }
-
-  Future<int> updateUser(User user) async {
-    final db = await instance.database;
-
-    return db.update(_userTableName, user.toJson(),
-        where: '${UserFields.id} = ?', whereArgs: [user.id]);
-  }
-
-  Future<int> deleteUser(int id) async {
-    final db = await instance.database;
-
-    return db.delete(_userTableName,
-        where: '${UserFields.id} = ?', whereArgs: [id]);
+    // TODO: create other tables
   }
 
   Future close() async {
