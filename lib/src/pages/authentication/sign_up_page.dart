f887a6aaf0166f8eb52fe30e49c6b3a30644b1d2
StasKrivosheya <stask49@gmail.com>
2023-05-02 10:39:07 +0300

    Revamp the registration page

--- lib/src/pages/authentication/sign_up_page.dart
+++ lib/src/pages/authentication/sign_up_page.dart
@@ -1,44 +1,13 @@
 import 'package:contacts/src/services/repository/user_repository.dart';
-import 'package:contacts/src/validators/string_validator.dart';
-import 'package:contacts/src/models/user_model.dart';
+import 'package:contacts/src/widgets/authentication/auth_status.dart';
+import 'package:contacts/src/widgets/authentication/sign_up/bloc/sign_up_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sqflite/sqflite.dart';
 
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({Key? key, required this.title}) : super(key: key);
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<StatefulWidget> createState() => _SignUpPageState();
-}
-
-class _SignUpPageState extends State<SignUpPage> {
-  final TextEditingController _loginController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController =
-      TextEditingController();
-
-  bool _canSignUp = false;
-
-  @override
-  void initState() {
-    _loginController.addListener(_updateSignUpButton);
-    _passwordController.addListener(_updateSignUpButton);
-    _confirmPasswordController.addListener(_updateSignUpButton);
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _loginController.dispose();
-    _passwordController.dispose();
-    _confirmPasswordController.dispose();
-
-    super.dispose();
-  }
+  final String title = 'Users SignUp';
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +22,51 @@ class _SignUpPageState extends State<SignUpPage> {
                 },
                 icon: const Icon(Icons.arrow_back)),
             const SizedBox(width: 15),
-            Text(widget.title),
+            Text(title),
           ],
         ),
       ),
-      body: SafeArea(
+      body: RepositoryProvider(
+        create: (context) => UserRepository(),
+        child: BlocProvider(
+          create: (context) => SignUpBloc(context.read<UserRepository>()),
+          child: const _SignUpLayout(),
+        ),
+      ),
+    );
+  }
+}
+
+class _SignUpLayout extends StatelessWidget {
+  const _SignUpLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignUpBloc, SignUpState>(
+      listenWhen: (previous, current) =>
+          current.status.isError || current.status.isSuccess,
+      listener: (context, state) {
+        if (state.status.isSuccess) {
+          Navigator.of(context).pop(state.username);
+        } else {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: const Text('SignUp error!'),
+                  content: Text(state.errorMessages.join('\n')),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Ok')),
+                  ],
+                );
+              });
+        }
+      },
+      child: SafeArea(
         child: Column(
           children: [
             Expanded(
@@ -70,21 +79,9 @@ class _SignUpPageState extends State<SignUpPage> {
                 margin: const EdgeInsets.symmetric(horizontal: 35),
                 child: Column(
                   children: [
-                    TextField(
-                      controller: _loginController,
-                      decoration: const InputDecoration(hintText: "Login"),
-                    ),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(hintText: "Password"),
-                    ),
-                    TextField(
-                      controller: _confirmPasswordController,
-                      obscureText: true,
-                      decoration:
-                          const InputDecoration(hintText: "Confirm Password"),
-                    ),
+                    _LoginInput(),
+                    _PasswordInput(),
+                    _ConfirmPasswordInput(),
                   ],
                 ),
               ),
@@ -94,28 +91,7 @@ class _SignUpPageState extends State<SignUpPage> {
               child: Container(
                 margin: const EdgeInsets.symmetric(horizontal: 35),
                 alignment: Alignment.center,
-                child: Opacity(
-                  opacity: _canSignUp ? 1 : 0.3,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 15),
-                      backgroundColor: const Color.fromRGBO(210, 105, 29, 1),
-                      disabledBackgroundColor:
-                          const Color.fromRGBO(210, 105, 29, 1),
-                      minimumSize: const Size.fromHeight(50),
-                      elevation: _canSignUp ? 5 : 0,
-                      side: const BorderSide(color: Colors.black),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(2)),
-                      ),
-                    ),
-                    onPressed: _canSignUp ? _signUpPressed : null,
-                    child: const Text(
-                      "SIGN UP",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                ),
+                child: _SignUpButton(),
               ),
             ),
           ],
@@ -123,91 +99,81 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
+}
 
-  void _updateSignUpButton() {
-    bool canSignIn = _loginController.text.isNotEmpty &&
-        _passwordController.text.isNotEmpty &&
-        _confirmPasswordController.text.isNotEmpty;
-    if (_canSignUp != canSignIn) {
-      setState(() {
-        _canSignUp = canSignIn;
-      });
-    }
+class _LoginInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (login) {
+        context.read<SignUpBloc>().add(SignUpUsernameChanged(login));
+      },
+      decoration: const InputDecoration(hintText: "Login"),
+    );
   }
+}
 
-  void _signUpPressed() async {
-    if (_passwordController.text == _confirmPasswordController.text) {
-      List<String> loginErrors =
-          StringValidator.validateLogin(_loginController.text);
-      List<String> passwordErrors =
-          StringValidator.validatePassword(_passwordController.text);
-      List<String> allErrors = loginErrors + passwordErrors;
+class _PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (password) {
+        context.read<SignUpBloc>().add(SignUpPasswordChanged(password));
+      },
+      obscureText: true,
+      decoration: const InputDecoration(hintText: "Password"),
+    );
+  }
+}
 
-      if (allErrors.isNotEmpty) {
-        showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text('Input error!'),
-                content: Text(allErrors.join('\n')),
-                actions: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Ok')),
-                ],
-              );
-            });
-      } else {
-        User user = User(
-            login: _loginController.text, password: _passwordController.text);
+class _ConfirmPasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (confirmPassword) {
+        context
+            .read<SignUpBloc>()
+            .add(SignUpConfirmPasswordChanged(confirmPassword));
+      },
+      obscureText: true,
+      decoration: const InputDecoration(hintText: "Confirm Password"),
+    );
+  }
+}
 
-        try {
-          //await DBProvider.instance.createUser(user);
-          UserRepository ur = context.read<UserRepository>();
-          int result = await ur.insertItemAsync(user);
-
-          if (context.mounted) {
-            Navigator.of(context).pop(user.login);
-          }
-        } on DatabaseException {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('Input error!'),
-                  content: Text(
-                      'Username ${_loginController.text} already exists, please consider another one.'),
-                  actions: [
-                    TextButton(
-                        onPressed: () {
-                          if (context.mounted) {
-                            Navigator.of(context).pop();
-                          }
-                        },
-                        child: const Text('Ok')),
-                  ],
-                );
-              });
-        }
-      }
-    } else {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: const Text('Input error!'),
-              content: const Text('Passwords are different!'),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text('Ok')),
-              ],
-            );
-          });
-    }
+class _SignUpButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<SignUpBloc, SignUpState>(
+      buildWhen: (previous, current) =>
+          previous.canProceedToSignUp != current.canProceedToSignUp,
+      builder: (context, state) {
+        return Opacity(
+          opacity: state.canProceedToSignUp ? 1 : 0.3,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              backgroundColor: const Color.fromRGBO(210, 105, 29, 1),
+              disabledBackgroundColor: const Color.fromRGBO(210, 105, 29, 1),
+              minimumSize: const Size.fromHeight(50),
+              elevation: state.canProceedToSignUp ? 5 : 0,
+              side: const BorderSide(color: Colors.black),
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(2)),
+              ),
+            ),
+            onPressed: state.canProceedToSignUp
+                ? () {
+                    context.read<SignUpBloc>().add(SignUpConfirmed());
+                  }
+                : null,
+            child: const Text(
+              "SIGN UP",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
