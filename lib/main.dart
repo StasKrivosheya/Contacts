4678980869dbf2a56843c716a50363dda9e6bf32
StasKrivosheya <stask49@gmail.com>
2023-05-02 13:26:54 +0300

    Move repositories declaration to the top

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:contacts/src/pages/authentication/sign_in_page.dart';
 import 'package:contacts/src/helpers/app_settings.dart';
 import 'package:contacts/src/pages/main_list_page.dart';
+import 'package:contacts/src/services/authentication/authentication_service.dart';
+import 'package:contacts/src/services/authentication/i_authentication_service.dart';
+import 'package:contacts/src/services/repository/user_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,20 +17,28 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final IAuthenticationService _authenticationService = const AuthenticationService();
+
   @override
   Widget build(BuildContext context) {
-    String? usersLogin = AppSettings.getLogin();
+    bool isLoggedIn = _authenticationService.isAuthorized;
 
-    return MaterialApp(
-      title: 'Contacts',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: _authenticationService),
+        RepositoryProvider(create: (context) => UserRepository()),
+      ],
+      child: MaterialApp(
+        title: 'Contacts',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: isLoggedIn ? '/mainList' : '/signIn',
+        routes: {
+          '/signIn': (context) => const SignInPage(),
+          '/mainList': (context) => const MainListPage(title: "Main List"),
+        },
       ),
-      initialRoute: usersLogin == null ? '/signIn' : '/mainList',
-      routes: {
-        '/signIn': (context) => const SignInPage(),
-        '/mainList': (context) => const MainListPage(title: "Main List"),
-      },
     );
   }
 }
